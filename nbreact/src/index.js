ff3abd2499f1ba356623887eb21a89c90c867d1c
DESKTOP-P6QN5A2\admin <1328693281@qq.com>
2020-11-20 10:07:43 +0800

    123124

--- nbreact/src/index.js
+++ nbreact/src/index.js
@@ -9,7 +9,8 @@ ReactDOM.render(
     <App />
   </React.StrictMode>,
   document.getElementById('root'),
-  console.log(Math.floor(Math.random()*10))
+  console.log(Math.floor(Math.random() * 10)),
+  console.log(21312432)
 );
 
 // If you want to start measuring performance in your app, pass a function
